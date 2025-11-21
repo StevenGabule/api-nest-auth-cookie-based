f638cc0db7426654c0ffcf12562f1065ec36ff7f
John Paul Gabule <lucas.gabule@gmail.com>
2025-11-21 23:20:20 +0800

    feature: adjust the refresh token

--- src/domain/auth/auth.controller.ts
+++ src/domain/auth/auth.controller.ts
@@ -64,7 +64,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 24 * 60 * 60 * 1000, // 1d
+      maxAge: 30 * 1000, // 30 seconds (for testing)
     });
 
     res.cookie('refresh_token', token.refresh_token, {
@@ -123,7 +123,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      maxAge: 30 * 1000, // 30 seconds (for testing)
     });
 
     res.cookie('refresh_token', tokens.refresh_token, {
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { createDocument } from './docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for frontend communication
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.js default port
+    credentials: true, // Allow cookies to be sent
+  });
+
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
   createDocument(app);
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 
 bootstrap().catch((err) => console.error(err));
